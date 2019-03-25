238e399d56096080f1e02d3fdd31bff39fdc24f9
shay silberstein <shay7566@gmail.com>
2019-03-25 14:44:03 +0200

    Add files via upload

---
+++ react-application-injectcss/lib/extensions/injectCss/InjectCssApplicationCustomizer.d.ts
@@ -0,0 +1,14 @@
+import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IInjectCssApplicationCustomizerProperties {
+    testMessage: string;
+    cssurl: string;
+}
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class InjectCssApplicationCustomizer extends BaseApplicationCustomizer<IInjectCssApplicationCustomizerProperties> {
+    onInit(): Promise<void>;
+}
